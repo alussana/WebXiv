f1974bedc2be3e96dcb0bbf51a9a919606b56c3a
alussana <alessandro.lussana@protonmail.com>
2021-06-28 10:33:27 +0100

    implemented contact functionality

--- www/contact/message.php
+++ www/contact/message.php
@@ -2,8 +2,9 @@
 $address = $_POST["address"];
 $name = $_POST["name"];
 $message = $_POST["message"];
-$txt = "NEW_MESSAGE\nname: ".$name."\naddress: ".$address."\n".$message."\n";
+$txt = "--\nname: ".$name."\naddress: ".$address."\n".$message;
 getcwd();
 file_put_contents('/usr/share/nginx/html/inbox/inbox.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
-header("Location: message.php");
+header("Location: sent.html");
+die();
 ?>
