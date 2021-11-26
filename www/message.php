03b683d177c94e17445f9433f3faa8ec2d818c38
EC2 Default User <ec2-user@ip-172-31-42-199.us-east-2.compute.internal>
2021-11-26 08:57:07 +0000

    updated pages; added date to mail messages

--- www/message.php
+++ www/message.php
@@ -1,8 +1,10 @@
 <?php
+date_default_timezone_set('Australia/Melbourne');
+$date = date('m/d/Y h:i:s a', time());
 $address = $_POST["address"];
 $name = $_POST["name"];
 $message = $_POST["message"];
-$txt = "--\nname: ".$name."\naddress: ".$address."\n".$message;
+$txt = "--\ndate: ".$date."|_|\nname: ".$name."|_|\naddress: ".$address."|_|\ntext: ".$message;
 getcwd();
 file_put_contents('/usr/share/nginx/html/inbox/inbox.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
 header("Location: sent.html");
